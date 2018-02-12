5edadbc6c18335df074066aa4271bc0854bc6c4f
VC Science Labs <vscilab@vcccd.edu>
2018-02-12 14:01:36 -0800

    Yo I changed your code homeboy

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,6 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
-//THIS IS A CHANGE
+//THIS IS A CHANGE. NO! THIS IS A CHANGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #include "stdafx.h"
 
 
